3f516e8cc7ecf9a905e9507e523f89ff59c60622
Joseph <Josephmburumungai99@gmail.com>
2023-11-10 18:36:44 +0300

    Customized the font and colors and added a favicon

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
+import { Unbounded } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const unbounded = Unbounded({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next Ap',
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={unbounded.className}>{children}</body>
     </html>
   )
 }
