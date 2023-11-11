56b252b17cbc8b0cb5ca39fe7e07e61b228e31b5
Joseph <Josephmburumungai99@gmail.com>
2023-11-11 14:40:35 +0300

    Created different pages and components

--- app/layout.js
+++ app/layout.js
@@ -1,19 +1,28 @@
 import { Unbounded } from 'next/font/google'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import './globals.css'
 
-const unbounded = Unbounded({ subsets: ['latin'] })
+
+const unbounded = Unbounded({ subsets: ['latin'] });
+
 
 export const metadata = {
   title: 'Andrew Gursky photography',
   description: 'Professional photography by Andrew Gurski. Specializing in landscape, portrait, and event photography.',
   keywords: 'Photography, Landscape, Portrait, Event, Professional',
-  author:'Andrew Gursky'    
-}
+  author: 'Andrew Gursky'
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={unbounded.className}>{children}</body>
+      <body className={unbounded.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
-  )
+  );
+
 }
