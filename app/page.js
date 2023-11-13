56eda14a4d37098332793f0d4bba9984a7ee2dad
Joseph <Josephmburumungai99@gmail.com>
2023-11-13 17:59:06 +0300

    updated homepage

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ function page() {
     <main className="text-text pt-20">
       <div className="text-center py-10">
         <p>Andrew Gursky</p>
-        <h1 className="text-5xl font-normal  md:text-7xl lg:text-9xl py-3">Photographer</h1>
+        <h1 className="text-4xl font-normal  md:text-7xl lg:text-9xl py-3">Photographer</h1>
       </div>
       <div>
         <PhotoCarousel />
