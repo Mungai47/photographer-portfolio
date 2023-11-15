d2733119888acdc004a7ccf0d5dd90fe23140b84
Joseph <Josephmburumungai99@gmail.com>
2023-11-15 21:37:28 +0300

    Created the gallery page

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import Image from 'next/image'
 
-function page() {
+function Page() {
+  const images = [
+    "/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg", "/images/image4.jpg", "/images/image8.jpg", "/images/image6.jpg", "/images/image7.jpg",
+    "/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg", "/images/image4.jpg", "/images/image8.jpg", "/images/image6.jpg", "/images/image7.jpg",
+    "/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg", "/images/image4.jpg", "/images/image8.jpg", "/images/image6.jpg", "/images/image7.jpg",
+    "/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg", "/images/image4.jpg", "/images/image8.jpg", "/images/image6.jpg", "/images/image7.jpg",
+    "/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg", "/images/image4.jpg", "/images/image8.jpg", "/images/image6.jpg", "/images/image7.jpg",
+    "/images/image1.jpg", "/images/image2.jpg", "/images/image3.jpg", "/images/image4.jpg", "/images/image8.jpg", "/images/image6.jpg", "/images/image7.jpg",
+  ] // Replace 'urlX' with your actual image URLs
+
   return (
     <div>
-      <p>Gallery page</p>
+      <p className="text-center text-2xl md:text-4xl lg:text-6xl underline underline-offset-8 text-text py-6">Gallery</p>
+      <div className="grid grid-cols-2  lg:grid-cols-4 gap-4 py-5">
+        {images.map((image, index) => (
+          <div key={index} className="transform transition duration-500 hover:scale-110">
+            <Image src={image} alt={`Gallery ${index}`} width={500} height={300} className="w-full h-auto rounded-lg" />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
 
-export default page
+export default Page
